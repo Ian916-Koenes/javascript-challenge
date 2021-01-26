c70216b2c8c81987c562e3c4ab849862e690fd91
Ian_Koenes <iankoenes@gmail.com>
2021-01-25 18:22:44 -0800

    added filters

--- static/js/app.js
+++ static/js/app.js
@@ -3,22 +3,30 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
+//select body
 var tbody = d3.select("tbody");
 
-data.forEach((UFOdata) => {
+// create function and generate UFO data
+function populate(inputData) {
+  inputData.forEach((UFOdata) => {
     var row = tbody.append("tr");
     Object.entries(UFOdata).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
-  });
-  
+  })};
+populate(tableData);
 
 // Select the button
 var button = d3.select("#filter-btn");
 
 // Select the form
 var form = d3.select("#datetime");
+var formCity = d3.select("#city");
+var formState = d3.select("#state");
+var formCountry = d3.select("#country");
+var formShape = d3.select("#shape");
+
 
 // Create event handlers 
 button.on("click", runSearch);
@@ -26,22 +34,33 @@ form.on("submit",runSearch);
 
 // Complete the event handler function for the form
 function runSearch() {
+    // Get the value property of the filter all
+    var filterAll = {
+      datetime: form.property("value").trim(),
+      city: formCity.property("value").toLowerCase().trim(),
+      state: formState.property("value").toLowerCase().trim(),
+      country: formCountry.property("value").toLowerCase().trim(),
+      shape: formShape.property("value").toLowerCase().trim()
+    };
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
     
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("datetime");
 
-     // Get the value property of the input element
-    var inputValue = inputElement.property("value");
+    //Place table data in filteredData
+    var filteredData = tableData;
 
-  console.log(inputValue);
-  console.log(tableData);
+    // Run each filter on the data
+    Object.entries(filterAll).forEach(([key, value]) => { if (value.length > 0) filteredData = filteredData.filter(sighting => value === sighting[key]);});
 
-  var filteredData = people.filter(person => person.bloodType === inputValue);
+    // clear body
+    tbody.html("");
 
-  console.log(filteredData);
+    //populate filtered data
+    populate(filteredData);
+
+    // log data
+    console.log(filteredData);
 
 
 };
